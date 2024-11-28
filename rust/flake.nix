03089f291438aee3363532af6067c1fbbaff851d
Dylan Bulfin <dylanbulfin@gmail.com>
2024-11-28 11:00:31 -0500

    Add fenix as input, toolchain support

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,46 +1,84 @@
 {
   description = "Basic Rust flake with home-manager module";
 
+  # inputs = { nixpkgs.url = "github:NixOS/nixpkgs/release-24.05"; };
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, }:
+  outputs = { self, fenix, nixpkgs, }:
     let
       system = "x86_64-linux";
       systemPkgs = nixpkgs.legacyPackages.${system};
 
-
-      # Change below sections, leave the rest alone
+      ####################################################################################
+      ## User Inputs:
       project = "project-name-here";
-      deps = with systemPkgs;[
-        # Put dependencies here
-      ];
-      native-deps = with systemPkgs;[
-        # Put native dependencies here
-      ];
+      version = "0.1.0";
 
+      # If you want to use a regular toolchain, set this to either: "stable", "beta",
+      # "nightly"
+      toolchainName = "stable";
 
-      package = systemPkgs.rustPlatform.buildRustPackage {
+      # If you want to use the toolchain specified by a file, set the file path here. If
+      # it is not null it will overwrite the toolchainName setting. One of these settings
+      # must be set
+      toolchainFile = null;
+      # If the toolchainFile is non-null it needs an SHA which it reads from here. When
+      # you first build it it will throw an error which will show the real SHA. Haven't
+      # found a better way around it yet
+      toolchainSha256 = systemPkgs.lib.fakeSha256;
+      deps = with systemPkgs;
+        [
+          # Put dependencies here
+        ];
+      native-deps = with systemPkgs;
+        [
+          # Put native dependencies here
+        ];
+
+      ## End of User Inputs
+      ####################################################################################
+
+      toolchain = if toolchainFile == null then
+        (if toolchainName == "stable" then
+          fenix.packages.${system}.stable.toolchain
+        else if toolchainName == "beta" then
+          fenix.packages.${system}.beta.toolchain
+        else if toolchainName == "nightly" then
+          fenix.packages.${system}.minimal.toolchain
+        else
+          abort "Can only select stable, beta, or nightly toolchain by name")
+      else
+        fenix.packages.${system}.fromToolchainFile {
+          file = toolchainFile;
+          sha256 = toolchainSha256;
+        };
+
+      package = (systemPkgs.makeRustPlatform {
+        cargo = toolchain;
+        rustc = toolchain;
+      }).buildRustPackage {
         pname = "${project}";
-        version = "0.1.0";
+        version = "${version}";
         src = ./.;
 
         buildInputs = deps;
 
         nativeBuildInputs = native-deps;
 
-        cargoLock = {
-          lockFile = ./Cargo.lock;
-        };
+        cargoLock = { lockFile = ./Cargo.lock; };
       };
       mod = { config, lib, pkgs, ... }:
         with lib;
         let
           cfg = config.programs.${project};
           tomlFormat = pkgs.formats.toml { };
-        in
-        {
+        in {
           options = {
             programs.${project} = {
               enable = mkEnableOption "${project}";
@@ -76,26 +114,20 @@
           config = mkIf cfg.enable {
             home.packages = [ cfg.package ];
 
-            xdg.configFile."${project}/config.toml" = lib.mkIf (cfg.settings != { }) {
-              source = tomlFormat.generate "config.toml" cfg.settings;
-            };
+            xdg.configFile."${project}/config.toml" =
+              lib.mkIf (cfg.settings != { }) {
+                source = tomlFormat.generate "config.toml" cfg.settings;
+              };
           };
         };
-    in
-    {
+    in {
       nixosModules.${project} = mod;
       nixosModules.default = self.nixosModules.${project};
 
       packages.${system}.default = package;
 
-      devShell = systemPkgs.mkShell {
-        buildInputs = deps ++ native-deps;
-      };
+      devShell = systemPkgs.mkShell { inputsFrom = [ package ]; };
 
-      overlays.default = (final: prev:
-        with final;
-        {
-          "${project}" = package;
-        });
+      overlays.default = (final: prev: with final; { "${project}" = package; });
     };
 }
